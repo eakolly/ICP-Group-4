2b5cb8fc806cc6aa972851359f61cf29cf2cd68b
papa-ofori <81566425+papa-ofori@users.noreply.github.com>
2022-01-21 09:12:10 -0900

    Add files via upload

---
+++ Nanakwasi-Boadi/hw0.cpp
@@ -0,0 +1,12 @@
+
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // print statement
+    cout << "Hello I am all setup for C++ for ICP";
+
+    return 0;
+}
