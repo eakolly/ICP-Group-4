b7810acd95c207fc7a98c334afe3fc9ed9e62417
Emmie-Ingabire <85745237+Emmie-Ingabire@users.noreply.github.com>
2022-01-21 13:29:15 +0000

    Delete Emerance_Ingabire/HW0 directory

--- Emerance_Ingabire/HW0/hello.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    cout << "Hello, I am all set up for c++ for ICP";
-    return 0;
-}
--- Emerance_Ingabire/HW0/hello.java
+++
@@ -1,6 +0,0 @@
-public class hello{
-    public static void main(String[]args){
-        System.out.println("Hello world, I am all set up for Java for ICP");
-
-    }
-}
